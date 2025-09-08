43bb2ce467031eab0230bceb426cb1cd22a59c1b
ke484 <o8o59223622@gmail.com>
2025-09-08 20:38:54 +0900

    計測タグ設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html>
+      <GoogleTagManager gtmId="G-KHQ4XYCNXZ" />
       <body>{children}</body>
     </html>
   );
